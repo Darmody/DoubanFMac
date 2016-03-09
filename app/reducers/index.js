720d2fdc138a77728c12609bcae09babbe1284d1
Darmody <eterlf41@gmail.com>
2016-03-10 00:10:31 +0800

    add redux-form

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as modal } from 'redux-modal';
+import {reducer as formReducer} from 'redux-form';
 import { routeReducer as routing } from 'react-router-redux';
 import auth from './auth';
 import doubanCaptcha from './doubanCaptcha';
 
 const rootReducer = combineReducers({
   routing,
+  form: formReducer,
   modal,
   auth,
   doubanCaptcha,
