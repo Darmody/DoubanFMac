d8346c38ffd46f605e3f1f4072a8c92d5782bd02
Darmody <eterlf41@gmail.com>
2016-03-09 09:48:01 +0800

    add redux-modal & react-modal

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as modal } from 'redux-modal';
 import { routeReducer as routing } from 'react-router-redux';
 import auth from './auth';
 
 const rootReducer = combineReducers({
   routing,
+  modal,
   auth,
 });
 
