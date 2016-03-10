9d10524a1d23bffbf5a9fc9e7b1583014651bdb0
Darmody <eterlf41@gmail.com>
2016-03-10 14:03:49 +0800

    add api-middleware-hook

--- app/store/configureStore.development.js
+++ app/store/configureStore.development.js
@@ -2,13 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import apiMiddlewareHook from 'reducers/middlewares/apiMiddlewareHook';
 import { hashHistory } from 'react-router';
 import { syncHistory } from 'react-router-redux';
 import rootReducer from '../reducers';
 
 const router = syncHistory(hashHistory);
 const logger = createLogger();
-const enhancer = applyMiddleware(thunk, router, apiMiddleware, logger);
+const enhancer = applyMiddleware(thunk, router, apiMiddlewareHook, apiMiddleware, logger);
 
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
--- app/store/configureStore.production.js
+++ app/store/configureStore.production.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
+import apiMiddlewareHook from 'reducers/middlewares/apiMiddlewareHook';
 import { hashHistory } from 'react-router';
 import { syncHistory } from 'react-router-redux';
 import rootReducer from '../reducers';
 
 const router = syncHistory(hashHistory);
-const enhancer = applyMiddleware(thunk, router, apiMiddleware);
+const enhancer = applyMiddleware(thunk, router, apiMiddlewareHook, apiMiddleware);
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
