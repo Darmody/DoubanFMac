60b91af7d1e97f8300c57ed3b2ed4bc4a406e430
Darmody <eterlf41@gmail.com>
2016-03-18 00:24:20 +0800

    fix log disappear bug

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -14,7 +14,7 @@ const middlewares = [
   thunk, router, apiMiddlewareHook, apiMiddleware, camelizeState
 ];
 
-if (process.env.NODE_ENV === 'developement') {
+if (process.env.NODE_ENV === 'development') {
   const createLogger = require('redux-logger');
   const logger = createLogger();
   middlewares.push(logger);
